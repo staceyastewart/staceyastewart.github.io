e0996073329f3068e81c51ff3df6aaefb4ddfa90
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-03-13 17:20:51 -0400

    finishes pseudocode

---
+++ project1/script.js
@@ -0,0 +1,31 @@
+console.log("linked")
+
+let body = $('body')
+
+
+//pseudocode
+//First, lay out the rules of the game:
+  //each player rolls 3 dice
+  //6 rounds, where the number of the round is the target for the number you want to roll on each die
+  //one point for each die matching the target
+  //if you get three of a kind of the target, you get 21 points
+  //if you roll three of a kind of anything but the target, you get 5 points
+  //each player keeps rolling in each round until a roll gives them zero points
+//One player vs the computer
+//have a function that randomly gives the player 3 numbers from 1-6 until no target numbers are showing
+  //the function must add the number of target numbers to the running score
+  //function must also acknowledge three of a kind (probably with an if/else statement)
+//probably use same function to get computer's scores
+//continue playing for 6 rounds
+//after 6 rounds, announce the winning player
+
+
+
+
+
+//grabbed the below from MDN
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
