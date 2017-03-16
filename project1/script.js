e895a63aa2d989ccee3f868b3b25bbe1398990dc
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-03-16 14:27:14 -0400

    finishes fully functioning 2 player game

--- project1/script.js
+++ project1/script.js
@@ -238,9 +238,9 @@ $("#button").on("click", function(event) {
 
      if(fullTurn_1_1[0][0] === fullTurn_1_1[0][1] && fullTurn_1_1[0][1] === fullTurn_1_1[0][2]) {
       if (fullTurn_1_1[0][0] === roundNumber) {
-        $("#messages").text("You rolled: "+ fullTurn_1_1[0][0] + ", " + fullTurn_1_1[0][1] + ", " + fullTurn_1_1[0][2] + ". Bunco! You scored 21 points! Please roll again.")
+        $("#messages").text("Player 1 rolled: "+ fullTurn_1_1[0][0] + ", " + fullTurn_1_1[0][1] + ", " + fullTurn_1_1[0][2] + ". Bunco! Player 1 scored 21 points! Please roll again.")
       } else {
-        $("#messages").text("You rolled: "+ fullTurn_1_1[0][0] + ", " + fullTurn_1_1[0][1] + ", " + fullTurn_1_1[0][2] + ". Three of a kind! You scored 5 points. Please roll again.")
+        $("#messages").text("Player 1 rolled: "+ fullTurn_1_1[0][0] + ", " + fullTurn_1_1[0][1] + ", " + fullTurn_1_1[0][2] + ". Three of a kind! Player 1 scored 5 points. Please roll again.")
       }
     }else {
       let scoreCount = 0
@@ -249,13 +249,13 @@ $("#button").on("click", function(event) {
           scoreCount++
           }
         }
-        $("#messages").text("You rolled: "+ fullTurn_1_1[0][0] + ", " + fullTurn_1_1[0][1] + ", " + fullTurn_1_1[0][2] + ". You scored " + scoreCount + ". Please roll again.")
+        $("#messages").text("Player 1 rolled: "+ fullTurn_1_1[0][0] + ", " + fullTurn_1_1[0][1] + ", " + fullTurn_1_1[0][2] + ". Player 1 scored " + scoreCount + ". Please roll again.")
       }
     fullTurn_1_1.shift()
 
     if (fullTurn_1_1.length == 0) {
       //if the array is empty, it is the next player's turn
-      $("#messages").text("Because you didn't roll a " + roundNumber + ", you didn't score on this turn. You scored " + round1Player1 + " points this round. It is now your opponent's turn.")
+      $("#messages").text("Because Player 1 didn't roll a " + roundNumber + ", Player 1 didn't score on this turn. Player 1 scored " + round1Player1 + " points this round. It is now Player 2's turn.")
       $("#button").css("visibility", "hidden")
       $(".gameInPlay:first").text(round1Player1);
       $(".gameInPlay:first").removeClass("gameInPlay")
@@ -276,9 +276,9 @@ $("#opponentButton").on("click", function(event) {
 
   if(fullTurn_1_2[0][0] === fullTurn_1_2[0][1] && fullTurn_1_2[0][1] === fullTurn_1_2[0][2]) {
     if (fullTurn_1_2[0][0] === roundNumber) {
-      $("#messages").text("They rolled: "+ fullTurn_1_2[0][0] + ", " + fullTurn_1_2[0][1] + ", " + fullTurn_1_2[0][2] + ". Bunco! Your opponent scored 21 points! Please roll again.")
+      $("#messages").text("Player 2 rolled: "+ fullTurn_1_2[0][0] + ", " + fullTurn_1_2[0][1] + ", " + fullTurn_1_2[0][2] + ". Bunco! Player 2 scored 21 points! Please roll again.")
     } else {
-      $("#messages").text("They rolled: "+ fullTurn_1_2[0][0] + ", " + fullTurn_1_2[0][1] + ", " + fullTurn_1_2[0][2] + ". Three of a kind! Your opponent scored 5 points. Please roll again.")
+      $("#messages").text("Player 2 rolled: "+ fullTurn_1_2[0][0] + ", " + fullTurn_1_2[0][1] + ", " + fullTurn_1_2[0][2] + ". Three of a kind! Player 2 scored 5 points. Please roll again.")
     }
   }else {
     let scoreCount = 0
@@ -287,11 +287,11 @@ $("#opponentButton").on("click", function(event) {
         scoreCount++
         }
       }
-      $("#messages").text("They rolled: "+ fullTurn_1_2[0][0] + ", " + fullTurn_1_2[0][1] + ", " + fullTurn_1_2[0][2] + ". Your opponent scored " + scoreCount + ". Please roll again.")
+      $("#messages").text("Player 2 rolled: "+ fullTurn_1_2[0][0] + ", " + fullTurn_1_2[0][1] + ", " + fullTurn_1_2[0][2] + ". Player 2 scored " + scoreCount + ". Please roll again.")
     }
   fullTurn_1_2.shift()
   if(fullTurn_1_2.length == 0) {
-    $("#messages").text("Because your opponent didn't roll a " + roundNumber + ", your opponent didn't score on this roll. They scored " + round1Player2 + " points this round. It's the end of this round. Please click 'Roll Your Dice' to begin the next round!")
+    $("#messages").text("Because Player 2 didn't roll a " + roundNumber + ", Player 2 didn't score on this roll. They scored " + round1Player2 + " points this round. It's the end of this round. Please click 'Roll Your Dice' to begin the next round!")
     console.log(round1Player2)
     $(".gameInPlay:first").text(round1Player2);
     $(".gameInPlay:first").removeClass("gameInPlay")
@@ -311,7 +311,6 @@ $("#opponentButton").on("click", function(event) {
 
 //Happens when the game finished and the end of game button is clicked
 $("#endOfGame").on("click", function(event) {
-    $("#round").css("visibility", "hidden")
     $("#button").hide()
     cardOne.remove();
     cardTwo.remove();
@@ -329,13 +328,16 @@ $("#endOfGame").on("click", function(event) {
     console.log(player2FinalScore)
     if (player1FinalScore > player2FinalScore) {
       //do something to alert the user they won
-      cardPlate.text("YOU WON!")
+      cardPlate.text("Player 1 won!")
+      $("#round").text("Congrats to Player 1!")
     } else if (player2FinalScore > player1FinalScore) {
       //do something to alert the user that they lost
-      cardPlate.text("You lost :(")
+      cardPlate.text("Player 2 won!")
+      $("#round").text("Congrats to Player 2!")
     } else {
       //let the user know they tied
       cardPlate.text("You tied!")
+      $("#round").text("Please play again!")
   }
 })
 
@@ -345,7 +347,6 @@ $("#endOfGame").on("click", function(event) {
 //
 // better alert for the conclusion of the game because it is anti-climactic
 // try to make the opponent's hand play without clicks
-//Do I actually need a readme file with explanations
 
 
 
