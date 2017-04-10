c222639beea5ce5faa9621e1cb4ecafc23db95b3
Stacey Stewart <stacey.alicia.stewart@gmail.com>
2017-04-09 21:23:29 -0400

    adds 2 more APIs

--- project2/db/schema.sql
+++ project2/db/schema.sql
@@ -1,20 +1,21 @@
 -- db name is tennis
-DROP TABLE IF EXISTS comments;
-DROP TABLE IF EXISTS posts;
-DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS comments;
+-- DROP TABLE IF EXISTS posts;
+-- DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS courts;
+DROP TABLE IF EXISTS onlineCourts;
+DROP TABLE IF EXISTS tennisPermits;
 
 
-
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  password_digest VARCHAR(255) NOT NULL,
-  borough VARCHAR(50) NOT NULL,
-  level VARCHAR(5) NOT NULL
-);
+-- CREATE TABLE users (
+--   id SERIAL PRIMARY KEY,
+--   first_name VARCHAR(50) NOT NULL,
+--   last_name VARCHAR(50) NOT NULL,
+--   email VARCHAR(255) UNIQUE NOT NULL,
+--   password_digest VARCHAR(255) NOT NULL,
+--   borough VARCHAR(50) NOT NULL,
+--   level VARCHAR(5) NOT NULL
+-- );
 
 CREATE TABLE courts (
   id SERIAL PRIMARY KEY,
@@ -24,21 +25,36 @@ CREATE TABLE courts (
   court_zip_code VARCHAR(50)
 );
 
-CREATE TABLE posts (
+-- CREATE TABLE posts (
+--   id SERIAL PRIMARY KEY,
+--   user_id INTEGER REFERENCES users(id),
+--   title VARCHAR(100) NOT NULL,
+--   content VARCHAR(550) NOT NULL,
+--   category VARCHAR(100) NOT NULL,
+--   level VARCHAR(5) NOT NULL,
+--   borough VARCHAR(50) NOT NULL
+-- );
+
+-- CREATE TABLE comments (
+--   id SERIAL PRIMARY KEY,
+--   user_id INTEGER REFERENCES users(id),
+--   post_id INTEGER REFERENCES posts(id),
+--   content VARCHAR(450) NOT NULL,
+--   level VARCHAR(5) NOT NULL,
+--   borough VARCHAR(50) NOT NULL
+-- );
+
+CREATE TABLE onlineCourts (
   id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id),
-  title VARCHAR(100) NOT NULL,
-  content VARCHAR(550) NOT NULL,
-  category VARCHAR(100) NOT NULL,
-  level VARCHAR(5) NOT NULL,
-  borough VARCHAR(50) NOT NULL
+  first_reservation VARCHAR(150),
+  last_reservation VARCHAR(150),
+  location VARCHAR(100),
+  reservation_courts VARCHAR(10),
+  walk_on_courts VARCHAR(10)
 );
 
-CREATE TABLE comments (
+CREATE TABLE tennisPermits (
   id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id),
-  post_id INTEGER REFERENCES posts(id),
-  content VARCHAR(450) NOT NULL,
-  level VARCHAR(5) NOT NULL,
-  borough VARCHAR(50) NOT NULL
+  age_group VARCHAR(150),
+  fee VARCHAR(10)
 );
